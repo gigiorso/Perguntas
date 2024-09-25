a6a12409f133d7dfa2463351a75c354cfbde0a6b
gigiorso <148974824+gigiorso@users.noreply.github.com>
2024-09-25 16:46:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Álbum',
+    'Qual o álbum mais famoso da banda Guns N´Roses ?',
+    'Appetite for Destruction.'
+)
+
+criaCartao(
+    'Música',
+    'Em que ano Modern Talking lançou sua primeira música, e qual era o nome dela?',
+    'O Modern Talking lançou o seu primeiro sucesso, "You`re My Heart, You`re My Soul", em 1984.'
+)
+
+criaCartao(
+    'Álbum',
+    'Em que ano o álbum Highway to Hell, da banda ACDC foi lançado?',
+    'No ano de 1979.'
+)
+
+criaCartao(
+    'Música',
+    'Qual o sentido da música November Rain, da banda Guns N´Roses?',
+    'Pode ser interpretada como uma metáfora para um período de tristeza e dificuldades em um relacionamento. A chuva de novembro, frequentemente fria e implacável, simboliza as adversidades que podem esfriar o amor entre duas pessoas.'
+)
